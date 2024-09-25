77b914b505d5db5ccf754ae756a957f307e0aa38
Shyam Ambilkar <shyamambilkar91@gmail.com>
2024-09-25 10:08:40 +0530

    Flask Sql Application

--- 12_Module/app.py
+++ 12_Module/app.py
@@ -1,19 +1,21 @@
+from doctest import debug
+
 from flask import Flask, request, flash, url_for, redirect,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://students_3.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = 'random string'
 
 db = SQLAlchemy(app)
 
-class student_3(db.Model):
+class students(db.Model):
     id = db.Column('student_id',db.Integer, primary_key = True)
-    name = db.Column(db.string(100))
-    city = db.Column(db.string(100))
-    addr = db.Column(db.string(100))
-    pin = db.Column(db.string(10))
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    addr = db.Column(db.String(100))
+    pin = db.Column(db.String(10))
 
 def __init__(self, name, city, addr, pin):
     self.name = name
@@ -24,8 +26,24 @@ def __init__(self, name, city, addr, pin):
 
 @app.route('/')
 def show_all():
-    return render_template('show_all.html', students = student_3.query.all())
+    return render_template('show_all.html', students = students.query.all())
 
+@app.route('/new',methods=['GET','POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+            flash('Please Enter all the Fields','error')
+        else:
+            student = students(request.form['name'], request.form['city'],request.form['addr'], request.form['pin'])
+
+            db.session.add(student)
+            db.session.commit()
+
+            flash("Record added Successfully...!!")
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
 
 if __name__=="__main__":
-    app.run()
+    # students.query.filter_by(city='akola').all()
+    # db.create_all()
+    app.run(debug=True)
